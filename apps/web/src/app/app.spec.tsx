506b4113f957c274e239acc605e33c2889fbc1f6
Bernard065 <bebenibernard@gmail.com>
2026-07-16 05:34:40 -0500

    fix: wrap App in MemoryRouter for tests
    App now uses react-router internally; tests need a Router context
    to avoid 'useRoutes() may be used only in context of a Router' error

--- apps/web/src/app/app.spec.tsx
+++ apps/web/src/app/app.spec.tsx
@@ -1,15 +1,24 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 
 import App from './app';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
-    const { getAllByText } = render(<App />);
+    const { getAllByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     expect(getAllByText(new RegExp('WanderBook', 'gi')).length > 0).toBeTruthy();
   });
 });
